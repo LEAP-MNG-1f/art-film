7bad5ad253a140b23b0cb55e8e9909a0e8178bbe
Отгонбаатар Баттөмөр <24LP0022@aiRo.local>
2024-12-07 19:03:20 -0800

    strapi connection succes

---
+++ backend/strapi/src/api/dreamers-day/controllers/dreamers-day.ts
@@ -0,0 +1,7 @@
+/**
+ * dreamers-day controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::dreamers-day.dreamers-day');
---
+++ backend/strapi/src/api/dreamers-day/routes/dreamers-day.ts
@@ -0,0 +1,7 @@
+/**
+ * dreamers-day router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::dreamers-day.dreamers-day');
