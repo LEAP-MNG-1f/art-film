9c703b9e4fe36a2bb48b2239f7859647cec2f859
Төгөлдөр Гантөмөр <24LP4099@G401-CC0212.local>
2024-12-10 15:18:50 +0800

    Home zasjiin

--- frontend-admin/app/_components/mainpage/maincontainer/MainContainer.tsx
+++ frontend-admin/app/_components/mainpage/maincontainer/MainContainer.tsx
@@ -1,5 +1,36 @@
-import { Header } from "./Header";
-
 export const MainContainer = () => {
-  return <div className="w-full">Main Container</div>;
+  return (
+    <div className="w-full">
+      <div></div>
+      <div className="grid grid-cols-3 gap-16 px-5 pt-5">
+        <div className="flex flex-col gap-5">
+          <div className="text-2xl">Dreamer's Day</div>
+          <div className="flex bg-slate-900 h-[300] text-white">
+            <div className="text-orange-400 text-[300px] leading-none px-2">
+              A
+            </div>
+            <div className="flex w-full h-full bg-orange-400"></div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-5">
+          <div className="text-2xl">Movies</div>
+          <div className="flex bg-slate-900 h-[300] text-white">
+            <div className="text-orange-400 text-[300px] leading-none px-2">
+              R
+            </div>
+            <div className="flex w-full h-full bg-orange-400"></div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-5">
+          <div className="text-2xl">Feedback</div>
+          <div className="flex bg-slate-900 h-[300] text-white">
+            <div className="text-orange-400 text-[300px] leading-none px-2">
+              T
+            </div>
+            <div className="flex w-full h-full bg-orange-400"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
