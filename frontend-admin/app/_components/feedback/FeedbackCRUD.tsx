9012f454cf641da97257b8fa880795ec8cb435c3
Hustle999 <tuguutuugii02@gmail.com>
2024-12-14 17:55:30 +0800

    vercel deer deploy hiisen

--- frontend-admin/app/_components/feedback/FeedbackCRUD.tsx
+++ frontend-admin/app/_components/feedback/FeedbackCRUD.tsx
@@ -1,7 +1,3 @@
-import { FeedbackCreate } from "./FeedbackCreate";
-import { FeedbackRead } from "./FeedbackRead";
-import { MockData } from "./MockDataFeed";
-
 export const FeedbackCRUD = () => {
   return <div>CRUD</div>;
 };
